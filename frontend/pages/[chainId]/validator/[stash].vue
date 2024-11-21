3887d7c681732d0ae8d480ddc5b3f8ba948e096f
Derek Colley <derek@colley.cc>
2024-11-21 11:28:39 +0000

    additional rules

--- frontend/pages/[chainId]/validator/[stash].vue
+++ frontend/pages/[chainId]/validator/[stash].vue
@@ -16,7 +16,7 @@
 
     <!-- {{ node }} -->
     <v-card>
-      <!-- <v-card-title>{{ node.identity }}</v-card-title> -->
+      <v-card-title>DN</v-card-title>
       <v-card-text>
         <v-list>
           <v-list-item>
@@ -43,12 +43,75 @@
             <v-list-item-title>{{ isNominated(node.stash) }}</v-list-item-title>
           </v-list-item> -->
 
-          <v-list-item v-show="telemetryError">
-            <v-list-item-subtitle text-color="red">Telemetry Error</v-list-item-subtitle>
-            <v-list-item-title>{{ telemetryError }}</v-list-item-title>
+          <v-list-item>
+            <v-card>
+              <v-row>
+                <v-col>
+                  <v-icon :color="hasTelemetry ? 'green' : 'red'">mdi-chart-box-outline</v-icon>
+                  Telemetry
+                </v-col>
+                <v-col>
+                  <v-icon :color="rulesBonded ? 'green' : 'red'">mdi-lock-outline</v-icon>
+                  Bonded
+                </v-col>
+                <v-col>
+                  <v-icon :color="rulesRewardDestingation ? 'green' : 'red'">mdi-hand</v-icon>
+                  Rewards
+                </v-col>
+                <v-col>
+                  <v-icon :color="rulesCommission ? 'green' : 'red'">mdi-percent</v-icon>
+                  Commission
+                </v-col>
+              </v-row>
+            </v-card>
+            <!-- <v-list-item-subtitle text-color="red">Has Telemetry</v-list-item-subtitle> -->
+            <!-- <v-list-item-title>{{ telemetryError }}</v-list-item-title> -->
           </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
 
-          <v-list-item v-show="telemetry?.TelemetryName">
+    <v-card>
+      <v-card-title>On Chain</v-card-title>
+      <v-card-text>
+        <v-list>
+          <v-list-item>
+            <v-list-item-subtitle>Account</v-list-item-subtitle>
+            <v-list-item-title>{{ toCoin(account.data?.free || 0) }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-subtitle>Bonded</v-list-item-subtitle>
+            <v-list-item-title>
+              <p v-for="lock in locks">
+                {{ lock.id }}: {{ toCoin(lock.amount) }}
+              </p>
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-subtitle>Reward Destination</v-list-item-subtitle>
+            <v-list-item-title>{{ rewardDestination }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-subtitle>Commission</v-list-item-subtitle>
+            <v-list-item-title>{{ (commission.commission || 0) / 10_000_000 }}%</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card-text>
+    </v-card>
+
+    <v-card>
+      <v-card-title>Telemetry</v-card-title>
+      <v-card-text>
+        <v-list>
+          <span v-show="!hasTelemetry">
+            <v-list-item v-show="telemetryError">
+              <v-list-item-subtitle text-color="red">Telemetry Error</v-list-item-subtitle>
+              <v-list-item-title>{{ telemetryError }}</v-list-item-title>
+            </v-list-item>
+          </span>
+
+          <span v-show="hasTelemetry">
+            <v-list-item v-show="telemetry?.TelemetryName">
             <v-list-item-subtitle>Telemetry Name</v-list-item-subtitle>
             <v-list-item-title>{{ telemetry?.TelemetryName }}</v-list-item-title>
           </v-list-item>
@@ -105,8 +168,8 @@
             </v-list-item-title>
           </v-list-item>
 
+          </span>
         </v-list>
-
       </v-card-text>
     </v-card>
 
@@ -115,13 +178,13 @@
       <v-card-text>
         [DN] <a href="https://nodes.web3.foundation/rules" target="_blank">Rules</a> <br>
         <ul>
-          <li>Self bond</li>
-          <li>Rewards Target</li>
-          <li>Commission</li>
-          <li>Telemetry</li>
+          <li>Self bond <v-icon>mdi-check</v-icon></li>
+          <li>Rewards Target  <v-icon>mdi-check</v-icon></li>
+          <li>Commission  <v-icon>mdi-check</v-icon></li>
+          <li>Telemetry  <v-icon>mdi-check</v-icon></li>
           <li>Hardware - https://wiki.polkadot.network/docs/maintain-guides-how-to-validate-polkadot#requirements</li>
           <li>IP4 & IP6</li>
-          <li>Client Version (24 houra)</li>
+          <li>Client Version (24 hours)</li>
           <li>dedicatee machine</li>
           <li>No slashes</li>
           <li>Performance A/A+?</li>
@@ -232,6 +295,25 @@ query telemetry($chainId: String!, $name: String!) {
   }
 }`
 
+const decimals = {
+  polkadot: 10,
+  kusama: 12,
+}
+
+const tokens = {
+  polkadot: 'DOT',
+  kusama: 'KSM',
+}
+
+const rules = {
+  commission: 5,
+  rewardDestination: 'Staked',
+  selfBond: {
+    polkadot: 7500,
+    kusama: 125,
+  }
+}
+
 export default defineComponent({
   name: 'CohortHome',
   async setup() {
@@ -253,6 +335,12 @@ export default defineComponent({
     const validators = ref([])
     const telemetry = ref({})
     const telemetryError = ref<null|string>(null)
+
+    const account = ref({})
+    const locks = ref([])
+    const rewardDestination = ref({})
+    const commission = ref(0)
+
     const loading = ref({
       node: true,
       telemetry: true,
@@ -260,6 +348,37 @@ export default defineComponent({
 
     const isLoading = computed(() => {
       return loading.node || loading.telemetry
+    })
+
+    const hasTelemetry = computed(() => {
+      return telemetry.value && Object.keys(telemetry.value).length > 0
+    })
+
+    const rulesBonded = computed(() => {
+      let _bonded: Number = 0;
+      locks.value.forEach(l => {
+        // console.debug('lock', l.id, l.amount);
+        if (l.id === 'staking') {
+          // console.debug('staking lock', l.amount);
+          _bonded = Number(l.amount) / Math.pow(10, decimals[chainId.value])
+        } else {
+          console.debug('not staking lock', `|${l.id}|`);
+        }
+      })
+      console.debug('_bonded', _bonded, locks.value);
+      // const _bondedCoin = _bonded ? Number(_bonded.amount) / Math.pow(10, decimals[chainId.value]) : 0
+      // console.debug('rulesBonded', _bonded, rules.selfBond[chainId.value]);
+      return _bonded >= rules.selfBond[chainId.value]
+    })
+
+    const rulesRewardDestingation = computed(() => {
+      return rewardDestination.value === rules.rewardDestination
+    })
+
+    const rulesCommission = computed(() => {
+      const _commission = (commission.value.commission || 0) / 10_000_000
+      // console.debug('rulesCommission', commission.value, _commission, rules.commission);
+      return _commission <= rules.commission
     })
 
     const reload = async () => {
@@ -310,7 +429,7 @@ export default defineComponent({
         telemetry.value = result.data?.telemetryByName?.NodeDetails || {};
         telemetryError.value = result.data?.telemetryByName?.NodeDetails 
           ? null 
-          : `No telemetry data found - make sure your node has '--name "${node.value.identity}"''`;
+          : `No telemetry data found - does your node have '--name "${node.value.identity}"''`;
       });
 
       // get Acount details
@@ -326,8 +445,26 @@ export default defineComponent({
     const getAccount = async () => {
       // if (!api) return
       api = await $substrate.getApi(chainId.value)
-      const account = await api.query.system.account(stash.value)
+      const _account = await api.query.system.account(stash.value)
       console.log('account', account)
+      account.value = _account
+
+      const _locks = await api.query.balances.locks(stash.value);
+      const _locksStr = _locks.toHuman()
+      for (const lock of _locksStr) {
+        console.debug('lock amount', lock.amount)
+        lock.id = lock.id.trim()
+        lock.amount = BigInt(lock.amount.replaceAll(',',''))
+      }
+      locks.value = _locksStr
+      console.debug('locks', locks.value)
+
+      const _rewardDestination = await api.query.staking.payee(stash.value)
+      rewardDestination.value = _rewardDestination.toHuman()
+
+      const _commission = await api.query.staking.validators(stash.value)
+      commission.value = _commission.toJSON()
+
     }
 
     const getStatus = (stash: string) => {
@@ -345,12 +482,33 @@ export default defineComponent({
       return nominators.value.find(n => n.stash === stash)
     }
 
+    const toCoin = (value: BigInt) => {
+      // if value is string, convert to number
+      if (typeof value === 'string') {
+        value = value.replace(',', '')
+        value = Number(value)
+      }
+      // console.debug('toCoin', value, chainId.value);
+      const ret = Number(value) / Math.pow(10, decimals[chainId.value])
+      return ret.toFixed(2) + ' ' + tokens[chainId.value]
+    }
+
     return {
       isLoading,
       reload,
       chainId,
       stash,
+
+      account,
+      locks,
+      rewardDestination,
+      commission,
+      rulesBonded,
+      rulesRewardDestingation,
+      rulesCommission,
+
       node,
+      hasTelemetry,
       telemetry,
       telemetryError,
       nominators,
@@ -359,6 +517,7 @@ export default defineComponent({
       // data,
       getStatus,
       isNominated,
+      toCoin,
     }
   }
 })
